cb50bfb9740e5771f1d0fdd97ee8160afa5b933c
alejovelez10 <alejovelez10@gmail.com>
2018-10-19 17:19:49 -0500

    organizando runners

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
-    belongs_to :runner, optional: true
-    belongs_to :country
+    belongs_to :runner
+    belongs_to :country, optional: true
     mount_uploader :images, PropertyUploader
 end
