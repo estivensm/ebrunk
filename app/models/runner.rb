1f196998b7cc037048ae1d4afd8e08b92a6a8b31
alejovelez10 <alejovelez10@gmail.com>
2018-10-19 17:15:51 -0500

    organizando runners

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,5 +1,6 @@
 class Runner < ApplicationRecord
   belongs_to :country
+ 
   has_many :property
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
