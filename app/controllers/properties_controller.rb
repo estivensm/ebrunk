eb1644bb8e7ffed39113f269e7fcaa24cb32e17f
estivensm <salazar-moncada-estiven@hotmail.com>
2018-10-20 09:43:33 -0500

    controller y index

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_runner!, :except => [:index]
+  before_action :authenticate_runner!, :except => [:index, :show]
   # GET /properties
   # GET /properties.json
   def index
