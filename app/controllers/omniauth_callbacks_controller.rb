6abd7f40c151f0617417240e18200df80631db70
alejovelez10 <alejovelez10@gmail.com>
2018-10-23 16:38:01 -0500

    autentication google parte 2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,9 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-  	puts request.env["omniauth.auth"]
-  	puts "ENVIROMENNNNNNNNNNNNNNNNNNNNNNN"
+
     @runner = Runner.find_for_google_oauth2(request.env["omniauth.auth"])
 
-    if @user
+    if @runner
       flash[:notice] = I18n.t "Tu autenticacion con Google calendar fue exitosa", :kind => "Google"
       sign_in_and_redirect @runner, :event => :authentication
     else
