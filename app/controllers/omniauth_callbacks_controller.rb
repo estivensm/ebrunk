e37ce9f29960b7ca9ebdabf2f6ccf2b4d4568f19
alejovelez10 <alejovelez10@gmail.com>
2018-10-23 16:54:22 -0500

    autentication google parte 3

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @runner
       puts "entreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
       flash[:notice] = I18n.t "Tu autenticacion con Google calendar fue exitosa", :kind => "Google"
-      sign_in_and_redirect @runner, :event => :authentication
+      sign_in_and_redirect root_path, :event => :authentication
     else
       puts "no entroooooooooooooooooooooooooooooooooooooooooooooooooooooo"
       redirect_to new_runner_session_path, notice: 'Esta cuenta no tiene Acceso'
