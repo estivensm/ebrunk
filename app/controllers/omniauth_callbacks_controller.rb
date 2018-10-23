a3608b73f10aa520cd3b3d34fccb4673227e42e2
alejovelez10 <alejovelez10@gmail.com>
2018-10-23 16:41:05 -0500

    autentication google parte 2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,14 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-
+    puts "siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
     @runner = Runner.find_for_google_oauth2(request.env["omniauth.auth"])
 
     if @runner
+      puts "entreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
       flash[:notice] = I18n.t "Tu autenticacion con Google calendar fue exitosa", :kind => "Google"
       sign_in_and_redirect @runner, :event => :authentication
     else
+      puts "no entroooooooooooooooooooooooooooooooooooooooooooooooooooooo"
       redirect_to new_runner_session_path, notice: 'Esta cuenta no tiene Acceso'
     end
   end
