197924d2c7cadd3ebe1fc42f82291bed7cdda41f
alejovelez10 <alejovelez10@gmail.com>
2018-10-24 12:49:38 -0500

    annotate

--- app/models/brunk_tracing.rb
+++ app/models/brunk_tracing.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: brunk_tracings
+#
+#  id         :integer          not null, primary key
+#  brunk_id   :integer
+#  runner_id  :integer
+#  comment    :text
+#  offer      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class BrunkTracing < ApplicationRecord
 end
