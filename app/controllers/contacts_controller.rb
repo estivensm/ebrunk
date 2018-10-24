c3704162d0f25780497d360fc3f2886a251c84a9
alejovelez10 <alejovelez10@gmail.com>
2018-10-24 18:15:55 -0500

    hello

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,47 +3,7 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.create(name: params[:name], email: params[:email], message: params[:message], call: params[:call],datetime_call: params[:datetime_call], state_call: params[:state_call], url_call: params[:url_call], contac_state: params[:contac_state], property_id: params[:property_id])
-    if @contact.save
-             contact = @contact
-     puts current_runner.token
-     puts "hollaaaaaaaaaaaaaaaaaaaaaaaaaa" if current_runner.token != nil && !current_runner.token.blank?
-     if current_runner.token != nil && !current_runner.token.blank?
-            
-            t = contact.datetime_call
-            t.min < 10 ? min = "0" : min = ""
-            t.hour < 10 ? hora = "0" : hora = ""
-            startdate = "#{t.year}-#{t.month}-#{t.day}T#{hora}#{t.hour}:#{min}#{t.min}:00-05:00"
 
-            te = t + (60*60)
-            te.min < 10 ? mint = "0" : mint = ""
-            te.hour < 10 ? horat = "0" : horat = ""
-            enddate = "#{te.year}-#{te.month}-#{te.day}T#{horat}#{te.hour}:#{mint}#{te.min}:00-05:00"
-            @event = {
-            'summary' => 'Video llamada',
-            'description' => "Video llamado para ver una propiedad",
-            'location' => contact.property.city,
-            'start' => { 'dateTime' => startdate },
-            'end' => {'dateTime' => enddate  },
-            'attendees' => [ { "email" => current_runner.email } ] ,
-            'conferenceData' => { "createRequest" => { "requestId" => "7qxalsvy0e"} }
-
-          }
-            client = Google::APIClient.new
-            client.authorization.refresh_token = current_runner.refresh_token_if_expired
-            client.authorization.access_token = current_runner.token
-            service = client.discovered_api('calendar', 'v3')
-            @set_event = client.execute(:api_method => service.events.insert,
-                                    :parameters => {'calendarId' => current_runner.email, 'sendNotifications' => true, "conferenceDataVersion" => 1},
-                                    :body => JSON.dump(@event),
-                                    :headers => {'Content-Type' => 'application/json'})
-            #@appointment.google_event_id = @set_event.data.id
-            puts "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            contact.url_call =  @set_event.data.hangoutLink
-            contact.save
-
-    end
-            
-    end  
   
   end
 
@@ -51,7 +11,7 @@ class ContactsController < ApplicationController
 
   def  crear_call
      
-     contact = @contact
+     contact = Contact.find(params[:id])
      puts current_runner.token
      puts "hollaaaaaaaaaaaaaaaaaaaaaaaaaa" if current_runner.token != nil && !current_runner.token.blank?
   	 if current_runner.token != nil && !current_runner.token.blank?
