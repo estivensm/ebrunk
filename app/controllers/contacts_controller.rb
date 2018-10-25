9bebab1872255bdb6a0f619ee1fd77b1881ec14e
alejovelez10 <alejovelez10@gmail.com>
2018-10-24 21:57:39 -0500

    hello

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -41,10 +41,10 @@ class ContactsController < ApplicationController
             client.authorization.refresh_token = current_runner.refresh_token_if_expired
             client.authorization.access_token = current_runner.token
             service = client.discovered_api('calendar', 'v3')
-            @set_event = client.execute(:api_method => service.events.insert,
-                                    :parameters => {'calendarId' => current_runner.email, 'sendNotifications' => true, "conferenceDataVersion" => 1},
-                                    :body => JSON.dump(@event),
-                                    :headers => {'Content-Type' => 'application/json'})
+            #@set_event = client.execute(:api_method => service.events.insert,
+             #                       :parameters => {'calendarId' => current_runner.email, 'sendNotifications' => true, "conferenceDataVersion" => 1},
+              #                      :body => JSON.dump(@event),
+               #                     :headers => {'Content-Type' => 'application/json'})
             #@appointment.google_event_id = @set_event.data.id
            
             #contact.url_call =  "holaaaaaaaaaaaaaaaaaaaa"
